acc5fb268136faa66e2d63f4b4b375c74cae293e
brayancastano <50753179+brayancastano@users.noreply.github.com>
2020-05-01 16:16:55 -0500

    front

--- src/main/webapp/app/layouts/footer/footer.component.ts
+++ src/main/webapp/app/layouts/footer/footer.component.ts
@@ -1,7 +1,18 @@
 import { Component } from '@angular/core';
+import { JhiLanguageService } from 'ng-jhipster';
+
+import { LANGUAGES } from 'app/core/language/language.constants';
 
 @Component({
   selector: 'jhi-footer',
   templateUrl: './footer.component.html'
 })
-export class FooterComponent {}
+export class FooterComponent {
+  languages = LANGUAGES;
+
+  constructor(private languageService: JhiLanguageService) {}
+
+  changeLanguage(languageKey: string): void {
+    this.languageService.changeLanguage(languageKey);
+  }
+}
